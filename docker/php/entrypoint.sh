dc36c5b3d7b145d5fcbd92db0e296c64d2309e75
Edvards <edvardskrumins@gmail.com>
2025-12-16 14:47:15 +0200

    run migrations in entrypoint

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Installing Composer dependencies"
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
+echo "Running migrations"
+php artisan migrate 
+
 echo "Starting Laravel Octane on port ${OCTANE_PORT} with ${OCTANE_WORKERS} workers and ${OCTANE_TASK_WORKERS} task workers"
 exec php artisan octane:start --server=swoole --host=0.0.0.0 --port=${OCTANE_PORT} --workers=${OCTANE_WORKERS} --task-workers=${OCTANE_TASK_WORKERS} 
 
